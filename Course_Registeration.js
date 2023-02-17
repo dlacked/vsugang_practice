3b7bf474c40b7d0adfcf861e973d5618715a72dd
Seo Heewon <seoheewon123@hufs.ac.kr>
2023-02-17 14:51:43 +0900

    1

--- Course_Registeration.js
+++ Course_Registeration.js
@@ -186,150 +186,168 @@ function deleteDone() {
   let abc = contirm('[해당 과목]을 삭제 하시겠습니까?')
 }
 
+//리스트 map을 통해 해당
 function done(clicked) {
   //몇번을 클릭했느냐에 따라 함수 호출
   cnt += 1 //몇번째로 클릭했느냐에 따라 번호 출력
   const item = document.getElementById('done')
   if (clicked === 1) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' + //innerHTML을 통해 테이블의 내용 출력
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>전공</td>' +
-      '<td>T01234567</td>' +
-      '<td>F</td>' +
-      '<td>MacOS</td>' +
-      '<td>3</td>' +
-      '<td>Bill Gates</td>' +
-      '<td>월 1 2 3 ( )</td>' +
-      '<td>0 / 100</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' + //innerHTML을 통해 테이블의 내용 출력
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>전공</td>' +
+        '<td>T01234567</td>' +
+        '<td>F</td>' +
+        '<td>MacOS</td>' +
+        '<td>3</td>' +
+        '<td>Bill Gates</td>' +
+        '<td>월 1 2 3 ( )</td>' +
+        '<td>0 / 100</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 2) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>전공</td>' +
-      '<td>T87654321</td>' +
-      '<td>F</td>' +
-      '<td>How to use Facebook</td>' +
-      '<td>3</td>' +
-      '<td>Mark Zuckerberg</td>' +
-      '<td>수 1 2 3 ( )</td>' +
-      '<td>0 / 60</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>전공</td>' +
+        '<td>T87654321</td>' +
+        '<td>F</td>' +
+        '<td>How to use Facebook</td>' +
+        '<td>3</td>' +
+        '<td>Mark Zuckerberg</td>' +
+        '<td>수 1 2 3 ( )</td>' +
+        '<td>0 / 60</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 3) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>전공</td>' +
-      '<td>S20212223</td>' +
-      '<td></td>' +
-      '<td>애플학개론</td>' +
-      '<td>3</td>' +
-      '<td>이재용</td>' +
-      '<td>수 4 5 ( )</td>' +
-      '<td>0 / 23</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>전공</td>' +
+        '<td>S20212223</td>' +
+        '<td></td>' +
+        '<td>애플학개론</td>' +
+        '<td>3</td>' +
+        '<td>이재용</td>' +
+        '<td>수 4 5 ( )</td>' +
+        '<td>0 / 23</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 4) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>전공</td>' +
-      '<td>I01023398</td>' +
-      '<td></td>' +
-      '<td>프론트엔드의 모든 것</td>' +
-      '<td>2</td>' +
-      '<td>임창용</td>' +
-      '<td>화 7 8 ( )</td>' +
-      '<td>0 / 14</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>전공</td>' +
+        '<td>I01023398</td>' +
+        '<td></td>' +
+        '<td>프론트엔드의 모든 것</td>' +
+        '<td>2</td>' +
+        '<td>임창용</td>' +
+        '<td>화 7 8 ( )</td>' +
+        '<td>0 / 14</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 5) {
-    item.innerHTML +=
+    doneList(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>과기</td>' +
-      '<td>T35140000</td>' +
-      '<td>F</td>' +
-      '<td>Tesla Mechanism</td>' +
-      '<td>2</td>' +
-      '<td>Elon Musk</td>' +
-      '<td>월 7 8 ( )</td>' +
-      '<td>0 / 100</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>과기</td>' +
+        '<td>T35140000</td>' +
+        '<td>F</td>' +
+        '<td>Tesla Mechanism</td>' +
+        '<td>2</td>' +
+        '<td>Elon Musk</td>' +
+        '<td>월 7 8 ( )</td>' +
+        '<td>0 / 100</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 6) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>생활</td>' +
-      '<td>U19900905</td>' +
-      '<td></td>' +
-      '<td>체육(피겨)</td>' +
-      '<td>1</td>' +
-      '<td>김연아</td>' +
-      '<td>금 3 4 ( )</td>' +
-      '<td>0 / 50</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>생활</td>' +
+        '<td>U19900905</td>' +
+        '<td></td>' +
+        '<td>체육(피겨)</td>' +
+        '<td>1</td>' +
+        '<td>김연아</td>' +
+        '<td>금 3 4 ( )</td>' +
+        '<td>0 / 50</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 7) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>인성</td>' +
-      '<td>B00000000</td>' +
-      '<td></td>' +
-      '<td>밥상머리인성교육</td>' +
-      '<td>2</td>' +
-      '<td></td>' +
-      '<td>금 5 6 ( )</td>' +
-      '<td>0 / 10</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>인성</td>' +
+        '<td>B00000000</td>' +
+        '<td></td>' +
+        '<td>밥상머리인성교육</td>' +
+        '<td>2</td>' +
+        '<td></td>' +
+        '<td>금 5 6 ( )</td>' +
+        '<td>0 / 10</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
   if (clicked === 8) {
-    item.innerHTML +=
+    doneList.push(
       '<tr><td>' +
-      cnt +
-      '</td>' +
-      '<td><button class="delete" onClick="deleteDone()"></button></td>' +
-      '<td>사철</td>' +
-      '<td>H19600000</td>' +
-      '<td></td>' +
-      '<td>한국외국어대학교사</td>' +
-      '<td>2</td>' +
-      '<td>박정운</td>' +
-      '<td>화 5 6 ( )</td>' +
-      '<td>0 / 20</td>' +
-      '<td><input type="checkbox"></td>' +
-      '<td></td></tr>'
+        cnt +
+        '</td>' +
+        '<td><button class="delete" onClick="deleteDone()"></button></td>' +
+        '<td>사철</td>' +
+        '<td>H19600000</td>' +
+        '<td></td>' +
+        '<td>한국외국어대학교사</td>' +
+        '<td>2</td>' +
+        '<td>박정운</td>' +
+        '<td>화 5 6 ( )</td>' +
+        '<td>0 / 20</td>' +
+        '<td><input type="checkbox"></td>' +
+        '<td></td></tr>'
+    )
+    item.innerHTML += doneList[doneList.length - 1]
   }
 }
 
 var count = 0
 let cnt = 0 //done()함수용 count 위랑 다릅니다
+const doneList = []
 var rc1 = 0
 var rc2 = 0
 var rc3 = 0
