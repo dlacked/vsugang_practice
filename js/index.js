222e47f8975e0fb833cdec0c4bebfb5fffffa5de
Changyong IM <imcy0104@gmail.com>
2024-01-04 02:13:07 +0900

    주석 추가

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ let clickedTM = document.getElementById('TPButton')
 let clickedSM = document.getElementById('SPButton')
 let clickedBM = document.getElementById('BButton')
 
-clickedTM.onclick = function(){
-    location.replace('./html/timeMode.html')
-}
+// clickedTM.onclick = function(){
+//     location.replace('./html/timeMode.html')
+// }
 
-clickedSM.onclick = function(){
-    location.replace('./html/signupMode.html')
-}
+// clickedSM.onclick = function(){
+//     location.replace('./html/signupMode.html')
+// }
 
-clickedBM.onclick = function(){
-    location.replace('./html/basicMode.html')
-}
+// clickedBM.onclick = function(){
+//     location.replace('./html/basicMode.html')
+// }
